cb9fe72eae86f900418ecf8b0bbcf86c17f8a126
firewalllee <lee_nguyen366@yahoo.com>
2016-09-21 12:00:26 +0700

    first

---
+++ Caves/modoverrides.lua
@@ -0,0 +1,78 @@
+return {
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=250 }, enabled=true },
+  ["workshop-375438821"]={
+    configuration_options={ EVERGREEN_CHOPS_NORMAL=3, EVERGREEN_CHOPS_SMALL=2, EVERGREEN_CHOPS_TALL=4 },
+    enabled=true 
+  },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=0,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=true,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-458940297"]={
+    configuration_options={
+      DFV_ClientPrediction="default",
+      DFV_FueledSettings="default",
+      DFV_Language="EN",
+      DFV_MinimalMode="default",
+      DFV_PercentReplace="default",
+      DFV_ShowACondition="default",
+      DFV_ShowADefence="default",
+      DFV_ShowAType="default",
+      DFV_ShowDamage="default",
+      DFV_ShowFireTime="default",
+      DFV_ShowInsulation="default",
+      DFV_ShowTemperature="default",
+      DFV_ShowUses="default" 
+    },
+    enabled=true 
+  },
+  ["workshop-503502935"]={ configuration_options={ time=10 }, enabled=true },
+  ["workshop-515467057"]={
+    configuration_options={ MAX_SPEED=1.25, MIN_SPEED=1, STEP_DOWN=0.004, STEP_UP=0.01 },
+    enabled=true 
+  },
+  ["workshop-623749604"]={
+    configuration_options={
+      Craft="Normal",
+      Destroyable="yees",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-680670714"]={ configuration_options={  }, enabled=true },
+  ["workshop-758532836"]={
+    configuration_options={
+      AUTOPAUSECONSOLE=false,
+      AUTOPAUSEMAP=false,
+      AUTOPAUSESINGLEPLAYERONLY=false,
+      ENABLECLIENTPAUSE=true,
+      ENABLEHOTKEY=false,
+      KEYBOARDTOGGLEKEY="P" 
+    },
+    enabled=true 
+  } 
+}
